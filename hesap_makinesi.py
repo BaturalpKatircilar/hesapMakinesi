01bd37efcc3073b7e838d7ad0c97648f3b07855d
Baturalp Katırcılar <153360837+BaturalpKatircilar@users.noreply.github.com>
2026-04-08 12:29:50 +0300

    Add files via upload

---
+++ hesap_makinesi.py
@@ -0,0 +1,34 @@
+#basit hesap makinesi
+sayi1 = float(input("İlk sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+print("işlemler")
+print("1:toplama")
+print("2:çıkarma")
+print("3:çarpma")
+print("4:bölme")
+print("5:üs alma")
+
+islem = input("bir işlem seçin: ")
+
+if (islem == "1"):
+    sonuç = sayi1 + sayi2
+    print(sonuç)
+elif (islem == "2"):
+    sonuç = sayi1 - sayi2
+    print(sonuç)
+elif (islem == "3"):
+    sonuç = sayi1 * sayi2
+    print(sonuç)
+elif (islem == "4"):
+    sonuç = sayi1 / sayi2
+    if (sayi2 != 0):
+        print(sonuç)
+    else:
+        print("error:number is zero")
+elif (islem == "5"):
+    sonuc = sayi1 ** sayi2
+    print(sonuc)
+
+else:
+    print("geçersiz sayılar")
